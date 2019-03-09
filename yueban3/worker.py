052a38b049767f2b4502909b9ddb9cba2cd03925
yangbo <yangbo1024@qq.com>
2019-03-09 11:31:27 +0800

    up

--- yueban3/worker.py
+++ yueban3/worker.py
@@ -277,7 +277,7 @@ def run(cfg_path, worker_app, reuse_port=False, settings={'KEEP_ALIVE': False}, 
     host = cfg["host"]
     port = cfg["port"]
     _web_app.config.update(settings)
-    if reuse_port:
+    if reuse_port and not kwargs.get('sock'):
         sock = socket.socket()
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
